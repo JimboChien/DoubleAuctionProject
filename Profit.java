6f149dc9ed3edd85267f4527973c00f22c54eb8a
KanKunI <jim60507@gmail.com>
2022-03-15 15:22:52 +0800

    update - modified profit algorithm

--- Profit.java
+++ Profit.java
@@ -4,46 +4,73 @@ import java.util.List;
 import java.util.Queue;
 
 public class Profit {
-    
-    private List<Point> traditionalSellerSupply = new ArrayList<Point>();
-    private List<Point> traditionalBuyerDemand = new ArrayList<Point>();
-    private List<Point> shiftedSellerSupply = new ArrayList<Point>();
-    private List<Point> modernBuyerDemand = new ArrayList<Point>();
 
-    public Profit(List<Point> traditionalSellerSupply, List<Point> traditionalBuyerDemand, List<Point> shiftedSellerSupply, List<Point> modernBuyerDemand) {
-        this.traditionalSellerSupply = traditionalSellerSupply;
-        this.traditionalBuyerDemand = traditionalBuyerDemand;
-        this.shiftedSellerSupply = shiftedSellerSupply;
-        this.modernBuyerDemand = modernBuyerDemand;
+    // private List<Point> traditionalSellerSupply = new ArrayList<Point>();
+    // private List<Point> traditionalBuyerDemand = new ArrayList<Point>();
+    // private List<Point> shiftedSellerSupply = new ArrayList<Point>();
+    // private List<Point> modernBuyerDemand = new ArrayList<Point>();
+
+    // public Profit(List<Point> traditionalSellerSupply, List<Point>
+    // traditionalBuyerDemand, List<Point> shiftedSellerSupply, List<Point>
+    // modernBuyerDemand) {
+    public Profit() {
+        // this.traditionalSellerSupply = traditionalSellerSupply;
+        // this.traditionalBuyerDemand = traditionalBuyerDemand;
+        // this.shiftedSellerSupply = shiftedSellerSupply;
+        // this.modernBuyerDemand = modernBuyerDemand;
     }
 
-    public double getTraditionalProfit( Point intersectPoint) {
+    public double getTraditionalProfit(Point intersectPoint) {
         double profit = 0;
 
-        // Calculate Seller Profit
-        for (int i = 2; i < traditionalSellerSupply.size(); i += 2) {
-            if (traditionalSellerSupply.get(i).getX() >= intersectPoint.getX() && traditionalSellerSupply.get(i - 1).getX() < intersectPoint.getX()) {
-                profit = DoubleMath.add(profit, DoubleMath.mul(Math.abs(DoubleMath.sub(traditionalSellerSupply.get(i).getY(), intersectPoint.getY())), DoubleMath.sub(intersectPoint.getX(), traditionalSellerSupply.get(i - 1).getX())));
-                break;
-            } else {
-                profit = DoubleMath.add(profit, DoubleMath.mul(Math.abs(DoubleMath.sub(traditionalSellerSupply.get(i).getY(), intersectPoint.getY())), DoubleMath.sub(traditionalSellerSupply.get(i).getX(), traditionalSellerSupply.get(i - 1).getX())));
-            }
-        }
+        profit = DoubleMath.mul(intersectPoint.getX(), intersectPoint.getY());
+        // // Calculate Seller Profit
+        // for (int i = 2; i < traditionalSellerSupply.size(); i += 2) {
+        // if (traditionalSellerSupply.get(i).getX() >= intersectPoint.getX()
+        // && traditionalSellerSupply.get(i - 1).getX() < intersectPoint.getX()) {
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(
+        // Math.abs(DoubleMath.sub(traditionalSellerSupply.get(i).getY(),
+        // intersectPoint.getY())),
+        // DoubleMath.sub(intersectPoint.getX(), traditionalSellerSupply.get(i -
+        // 1).getX())));
+        // break;
+        // } else {
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(
+        // Math.abs(DoubleMath.sub(traditionalSellerSupply.get(i).getY(),
+        // intersectPoint.getY())),
+        // DoubleMath.sub(traditionalSellerSupply.get(i).getX(),
+        // traditionalSellerSupply.get(i - 1).getX())));
+        // }
+        // }
 
-        // Calculate Buyer Profit
-        for (int i = 2; i < traditionalBuyerDemand.size(); i += 2) {
-            if(traditionalBuyerDemand.get(i).getX() >= intersectPoint.getX() && traditionalBuyerDemand.get(i - 1).getX() < intersectPoint.getX()) {
-                profit = DoubleMath.add(profit, DoubleMath.mul(Math.abs(DoubleMath.sub(traditionalBuyerDemand.get(i).getY(), intersectPoint.getY())), DoubleMath.sub(intersectPoint.getX(), traditionalBuyerDemand.get(i - 1).getX())));
-                break;
-            } else {
-                profit = DoubleMath.add(profit, DoubleMath.mul(Math.abs(DoubleMath.sub(traditionalBuyerDemand.get(i).getY(), intersectPoint.getY())), DoubleMath.sub(traditionalBuyerDemand.get(i).getX(), traditionalBuyerDemand.get(i - 1).getX())));
-            }
-         }
+        // // Calculate Buyer Profit
+        // for (int i = 2; i < traditionalBuyerDemand.size(); i += 2) {
+        // if (traditionalBuyerDemand.get(i).getX() >= intersectPoint.getX()
+        // && traditionalBuyerDemand.get(i - 1).getX() < intersectPoint.getX()) {
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(
+        // Math.abs(DoubleMath.sub(traditionalBuyerDemand.get(i).getY(),
+        // intersectPoint.getY())),
+        // DoubleMath.sub(intersectPoint.getX(), traditionalBuyerDemand.get(i -
+        // 1).getX())));
+        // break;
+        // } else {
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(
+        // Math.abs(DoubleMath.sub(traditionalBuyerDemand.get(i).getY(),
+        // intersectPoint.getY())),
+        // DoubleMath.sub(traditionalBuyerDemand.get(i).getX(),
+        // traditionalBuyerDemand.get(i - 1).getX())));
+        // }
+        // }
 
         return profit;
     }
 
-    public double getModernProfit( Point intersectPoint) {
+    // public double getModernProfit(Point intersectPoint) {
+    public double getModernProfit(List<Point> modernBuyerDemand, List<Point> shiftedSellerSupply) {
         double profit = 0;
         int currentSeller = 2;
         int prevSeller = 0;
@@ -54,46 +81,72 @@ public class Profit {
         double buyerRemain = 0;
         double temp = 0;
 
-        while (shiftedSellerSupply.get(currentSeller).getX() < 0) {
-            currentSeller += 2;
-        }
-        prevSeller = currentSeller;
-        while (currentSeller < shiftedSellerSupply.size()) {
-            // seller
-            if (shiftedSellerSupply.get(currentSeller - 1).getX() <= 0) {
-                sellerRemain = shiftedSellerSupply.get(currentSeller).getX();
-            } else if (currentSeller == prevSeller  ) {
-                sellerRemain = temp;
-            } else {
-                sellerRemain = DoubleMath.sub(shiftedSellerSupply.get(currentSeller).getX(), shiftedSellerSupply.get(currentSeller - 1).getX());
-                prevSeller = currentSeller;
-            }
-            // buyer
-            if (currentBuyer == prevBuyer) {
-                buyerRemain = temp;
-            } else {
-                buyerRemain = DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getX(), modernBuyerDemand.get(currentBuyer - 1).getX());
-                prevBuyer = currentBuyer;
-            }
+        // while (shiftedSellerSupply.get(currentSeller).getX() < 0) {
+        // currentSeller += 2;
+        // }
+        // prevSeller = currentSeller;
+        // while (currentSeller < shiftedSellerSupply.size()) {
+        // // seller
+        // if (shiftedSellerSupply.get(currentSeller - 1).getX() <= 0) {
+        // sellerRemain = shiftedSellerSupply.get(currentSeller).getX();
+        // } else if (currentSeller == prevSeller) {
+        // sellerRemain = temp;
+        // } else {
+        // sellerRemain = DoubleMath.sub(shiftedSellerSupply.get(currentSeller).getX(),
+        // shiftedSellerSupply.get(currentSeller - 1).getX());
+        // prevSeller = currentSeller;
+        // }
+        // // buyer
+        // if (currentBuyer == prevBuyer) {
+        // buyerRemain = temp;
+        // } else {
+        // buyerRemain = DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getX(),
+        // modernBuyerDemand.get(currentBuyer - 1).getX());
+        // prevBuyer = currentBuyer;
+        // }
 
-            if (sellerRemain > buyerRemain) {
-                temp = DoubleMath.sub(sellerRemain, buyerRemain);
-                profit = DoubleMath.add(profit, DoubleMath.mul(buyerRemain, DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(), shiftedSellerSupply.get(currentSeller).getY()))) ;
-                currentBuyer += 2;
-            } else if (sellerRemain < buyerRemain) {
-                temp = DoubleMath.sub(buyerRemain, sellerRemain);
-                profit = DoubleMath.add(profit, DoubleMath.mul(sellerRemain, DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(), shiftedSellerSupply.get(currentSeller).getY()))) ;
-                currentSeller += 2;
-            } else {
-                temp = 0;
-                profit = DoubleMath.add(profit, DoubleMath.mul(sellerRemain, DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(), shiftedSellerSupply.get(currentSeller).getY()))) ;
-                currentSeller += 2;
-                currentBuyer += 2;
-            }
+        // if (sellerRemain > buyerRemain) {
+        // temp = DoubleMath.sub(sellerRemain, buyerRemain);
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(buyerRemain,
+        // DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(),
+        // shiftedSellerSupply.get(currentSeller).getY())));
+        // currentBuyer += 2;
+        // } else if (sellerRemain < buyerRemain) {
+        // temp = DoubleMath.sub(buyerRemain, sellerRemain);
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(sellerRemain,
+        // DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(),
+        // shiftedSellerSupply.get(currentSeller).getY())));
+        // currentSeller += 2;
+        // } else {
+        // temp = 0;
+        // profit = DoubleMath.add(profit,
+        // DoubleMath.mul(sellerRemain,
+        // DoubleMath.sub(modernBuyerDemand.get(currentBuyer).getY(),
+        // shiftedSellerSupply.get(currentSeller).getY())));
+        // currentSeller += 2;
+        // currentBuyer += 2;
+        // }
 
-            // break
-            if (modernBuyerDemand.get(currentBuyer).getX() > shiftedSellerSupply.get(shiftedSellerSupply.size() - 1).getX()) {
-                break;
+        // // break
+        // if (modernBuyerDemand.get(currentBuyer).getX() >
+        // shiftedSellerSupply.get(shiftedSellerSupply.size() - 1)
+        // .getX()) {
+        // break;
+        // }
+        // }
+
+        for (int i = 2; i < shiftedSellerSupply.size() - 1; i += 2) {
+            if (shiftedSellerSupply.get(i - 1).getX() <= 0 && shiftedSellerSupply.get(i).getX() <= 0) {
+                continue;
+            } else if (shiftedSellerSupply.get(i - 1).getX() <= 0 && shiftedSellerSupply.get(i).getX() > 0) {
+                profit = DoubleMath.add(profit,
+                        DoubleMath.mul(shiftedSellerSupply.get(i).getX(), shiftedSellerSupply.get(i).getY()));
+            } else {
+                profit = DoubleMath.add(profit, DoubleMath.mul(
+                        DoubleMath.sub(shiftedSellerSupply.get(i).getX(), shiftedSellerSupply.get(i - 1).getX()),
+                        shiftedSellerSupply.get(i).getY()));
             }
         }
 
--- mainTest.java
+++ mainTest.java
@@ -27,8 +27,9 @@ public class mainTest {
         System.out.println("|    Generate Seller Data    |");
         System.out.println("==============================\n");
         for (Map.Entry<Integer, Double> entry : sellersPriceMap.entrySet()) {
-            System.out.println("Seller: " + entry.getKey() + "\tQuantity: " + originalSeller[entry.getKey()].getQuantity()
-                    + "\tPrice: " + entry.getValue());
+            System.out
+                    .println("Seller: " + entry.getKey() + "\tQuantity: " + originalSeller[entry.getKey()].getQuantity()
+                            + "\tPrice: " + entry.getValue());
         }
 
         originalBuyers = dataGenerator.getBuyers();
@@ -42,8 +43,9 @@ public class mainTest {
         System.out.println("|    Generate buyer Data     |");
         System.out.println("==============================\n");
         for (Map.Entry<Integer, Double> entry : buyersPricMap.entrySet()) {
-            System.out.println("Buyer: " + entry.getKey() + "\tQuantity: " + originalBuyers[entry.getKey()].getQuantity()
-                    + "\tPrice: " + entry.getValue());
+            System.out
+                    .println("Buyer: " + entry.getKey() + "\tQuantity: " + originalBuyers[entry.getKey()].getQuantity()
+                            + "\tPrice: " + entry.getValue());
         }
 
         /************************
@@ -70,16 +72,17 @@ public class mainTest {
         System.out.println("==============================\n");
         System.out.println("sortingSellersPrice:");
         sortingSellersPrice.forEach(result -> System.out.println("Key: " +
-        result.getKey() + "\t\tValue: " + result.getValue() + "\tQuantity: " +
-        originalSeller[result.getKey()].getQuantity()));
+                result.getKey() + "\t\tValue: " + result.getValue() + "\tQuantity: " +
+                originalSeller[result.getKey()].getQuantity()));
         System.out.println();
         System.out.println("sortingBuyersPrice:");
         sortingBuyersPrice.forEach(result -> System.out.println("Key: " +
-        result.getKey() + "\t\tValue: " + result.getValue() + "\tQuantity: " +
-        originalBuyers[result.getKey()].getQuantity()));
+                result.getKey() + "\t\tValue: " + result.getValue() + "\tQuantity: " +
+                originalBuyers[result.getKey()].getQuantity()));
 
         // for (int i = 0; i < modernSellerSupply.size(); i++) {
-        //     System.out.println(modernSellerSupply.get(i).getX() + "\t" + modernSellerSupply.get(i).getY());
+        // System.out.println(modernSellerSupply.get(i).getX() + "\t" +
+        // modernSellerSupply.get(i).getY());
         // }
 
         // Print Chart Point Result
@@ -90,7 +93,8 @@ public class mainTest {
         // }
         // System.out.println("traditionalSellerSupply");
         // for (int i = 0; i < traditionalSellerSupply.size(); i++) {
-        //     System.out.println("X: " + traditionalSellerSupply.get(i).getX() + "\t\tY: " + traditionalSellerSupply.get(i).getY());
+        // System.out.println("X: " + traditionalSellerSupply.get(i).getX() + "\t\tY: "
+        // + traditionalSellerSupply.get(i).getY());
         // }
 
         /************************
@@ -101,10 +105,12 @@ public class mainTest {
         System.out.println("===============================");
         System.out.println("|  Draw Chart & Save to Image |");
         System.out.println("===============================\n");
-        // DrawChart drawChartByTraditional = new DrawChart(traditionalSellerSupply, traditionalBuyerDemand);
+        // DrawChart drawChartByTraditional = new DrawChart(traditionalSellerSupply,
+        // traditionalBuyerDemand);
         // DrawChart drawChartByModern = new DrawChart(modernSellerSupply,
-        //         traditionalBuyerDemand);
-        DrawChart drawChart = new DrawChart(traditionalSellerSupply, traditionalBuyerDemand, modernSellerSupply, modernBuyerDemand);
+        // traditionalBuyerDemand);
+        DrawChart drawChart = new DrawChart(traditionalSellerSupply, traditionalBuyerDemand, modernSellerSupply,
+                modernBuyerDemand);
 
         double floor;
         double ceil;
@@ -112,28 +118,30 @@ public class mainTest {
 
         // Floor
         if (traditionalSellerSupply.get(1).getY() < traditionalBuyerDemand.get(traditionalBuyerDemand.size() - 2)
-        .getY()) {
+                .getY()) {
             floor = traditionalSellerSupply.get(1).getY();
         } else {
             floor = traditionalBuyerDemand.get(traditionalBuyerDemand.size() - 2).getY();
         }
-        
+
         // Ceiling
         if (traditionalSellerSupply.get(traditionalSellerSupply.size() - 2).getY() > traditionalBuyerDemand.get(1)
-        .getY()) {
+                .getY()) {
             ceil = traditionalSellerSupply.get(traditionalSellerSupply.size() - 2).getY();
         } else {
             ceil = traditionalBuyerDemand.get(1).getY();
         }
-        
+
         // Quantity
-        if (traditionalSellerSupply.get(traditionalSellerSupply.size() - 1).getX() > traditionalBuyerDemand.get(traditionalBuyerDemand.size() - 1).getX()) {
+        if (traditionalSellerSupply.get(traditionalSellerSupply.size() - 1).getX() > traditionalBuyerDemand
+                .get(traditionalBuyerDemand.size() - 1).getX()) {
             quantity = traditionalSellerSupply.get(traditionalSellerSupply.size() - 1).getX();
         } else {
             quantity = traditionalBuyerDemand.get(traditionalBuyerDemand.size() - 1).getX();
         }
 
-        // drawChartByTraditional.draw("Traditional_Supply_and_Demand_LineChart", floor, ceil);
+        // drawChartByTraditional.draw("Traditional_Supply_and_Demand_LineChart", floor,
+        // ceil);
         // drawChartByModern.draw("Modern_Supply_and_Demand_LineChart", floor, ceil);
         drawChart.draw(floor, ceil, quantity);
 
@@ -144,21 +152,29 @@ public class mainTest {
 
         Point traditionalIntersectPoint = drawChart.getTraditionalIntersectPoint();
         Point modernIntersectPoint = drawChart.getModernIntersectPoint();
-        System.out.println("Traditional Intersect Point: (" + traditionalIntersectPoint.getX() + ", " + traditionalIntersectPoint.getY() + ")");
-        System.out.println("Modern Intersect Point: (" + modernIntersectPoint.getX() + ", " + modernIntersectPoint.getY() + ")");
-        
+        System.out.println("Traditional Intersect Point: (" + traditionalIntersectPoint.getX() + ", "
+                + traditionalIntersectPoint.getY() + ")");
+        System.out.println(
+                "Modern Intersect Point: (" + modernIntersectPoint.getX() + ", " + modernIntersectPoint.getY() + ")");
+
         System.out.println();
         System.out.println("==============================");
         System.out.println("|      Calculate Profit      |");
         System.out.println("==============================\n");
-        
+
         List<Point> shiftedSellerSupply = drawChart.getShiftedSellerSupply();
-        Profit profit = new Profit(traditionalSellerSupply, traditionalBuyerDemand, shiftedSellerSupply, modernBuyerDemand);
+        // Profit profit = new Profit(traditionalSellerSupply, traditionalBuyerDemand,
+        // shiftedSellerSupply,
+        // modernBuyerDemand);
+        Profit profit = new Profit();
         System.out.println("Traditional Profit: " + profit.getTraditionalProfit(traditionalIntersectPoint));
-        System.out.println("Modern Profit: " + profit.getModernProfit(modernIntersectPoint));
-        // System.out.println("shiftedSellerSupply");
-        // for (int i = 0; i < shiftedSellerSupply.size(); i++) {
-        //     System.out.println("X: " + shiftedSellerSupply.get(i).getX() + "\t\tY: " + shiftedSellerSupply.get(i).getY());
+        // System.out.println("Modern Profit: " +
+        // profit.getModernProfit(modernIntersectPoint));
+        System.out.println("Modern Profit: " + profit.getModernProfit(modernBuyerDemand, shiftedSellerSupply));
+        System.out.println("modernBuyerDemand");
+        // for (int i = 0; i < modernBuyerDemand.size(); i++) {
+        // System.out.println("X: " + modernBuyerDemand.get(i).getX() + "\t\tY: " +
+        // modernBuyerDemand.get(i).getY());
         // }
 
         System.out.println("\n\n[+] Mission Completed !!!\n");
